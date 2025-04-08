fd16c4eb24ec798baa0e3fa933cdd1c5099a9cf0
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-04-08 20:42:02 -0300

    responsive

--- src/components/GenreMoveList.jsx
+++ src/components/GenreMoveList.jsx
@@ -5,7 +5,7 @@ const GenreMoveList = () => {
   return (
     <div>
         {GenresList.genere.map((item,index) => index<=4&& (
-            <div className="p-8 px-8 md:px-16">
+            <div className="pt-8 pb-2 px-8 md:px-16">
                 <h2 className="text-[20px] text-white font-bold">{item.name}</h2>
                 <MovieList genreId={item.id} index_ = {index}/>
             </div>
--- src/components/HrMovieCard.jsx
+++ src/components/HrMovieCard.jsx
@@ -3,12 +3,12 @@ const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
 
 function HrMovieCard({movie}) {
   return (
-    <section className='hover:scale-110 transition-all duration-150 ease-in'>
+    <section className='hover:scale-110 transition-all duration-200 ease-in'>
     <img src={IMAGE_BASE_URL+movie.backdrop_path} 
-    className='w-[110px] md:w-[260px] rounded-lg
+    className='w-[150px] md:w-[260px] rounded-lg
     hover:border-[3px] border-gray-400 cursor-pointer
     '/>
-    <h2 className='w-[110px] md:w-[260px] text-white
+    <h2 className='w-[150px] md:w-[260px] text-white
     mt-2'>{movie.title}</h2>
     </section>
   )
