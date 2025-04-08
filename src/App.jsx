a2dff52ceececb882e1cc71a6c9863b4fe20349a
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-04-08 18:36:04 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import GenreMoveList from './components/GenreMoveList'
+import Header from "./components/Header"
+import ProductionHouse from './components/ProductionHouse'
+import Slider from './components/Slider'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Slider/>
+      <ProductionHouse/>
+      <GenreMoveList/>
+    </div>
+  )
+}
+
+export default App
