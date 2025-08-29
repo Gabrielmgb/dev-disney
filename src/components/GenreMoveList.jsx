04543d87f2d0b4294c15ba5307e95a33e9524540
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-08-28 22:11:08 -0300

    Organized code from GenreMoveList

--- src/components/GenreMoveList.jsx
+++ src/components/GenreMoveList.jsx
@@ -1,17 +1,20 @@
-import GenresList from "../Constant/GenresList"
-import MovieList from "./MovieList"
+import GenresList from "../Constant/GenresList";
+import MovieList from "./MovieList";
 
 const GenreMoveList = () => {
   return (
     <div>
-        {GenresList.genere.map((item,index) => index<=4&& (
+      {GenresList.genere.map(
+        (item, index) =>
+          index <= 4 && (
             <div className="pt-8 pb-2 px-8 md:px-16">
-                <h2 className="text-[20px] text-white font-bold">{item.name}</h2>
-                <MovieList genreId={item.id} index_ = {index}/>
+              <h2 className="text-[20px] text-white font-bold">{item.name}</h2>
+              <MovieList genreId={item.id} index_={index} />
             </div>
-        ))}
+          )
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default GenreMoveList
+export default GenreMoveList;
