1a213912a073f7d27cca5b78d6287ab99ceb2a7c
OticasVisiotech <gabrielmgb.nd@gmail.com>
2025-10-02 22:34:20 -0300

    Organized code from main

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import './App.css'
-import GenreMoveList from './components/GenreMoveList'
-import Header from "./components/Header"
-import ProductionHouse from './components/ProductionHouse'
-import Slider from './components/Slider'
+import "./App.css";
+import GenreMoveList from "./components/GenreMoveList";
+import Header from "./components/Header";
+import ProductionHouse from "./components/ProductionHouse";
+import Slider from "./components/Slider";
 
 const App = () => {
   return (
     <div>
-      <Header/>
-      <Slider/>
-      <ProductionHouse/>
-      <GenreMoveList/>
+      <Header />
+      <Slider />
+      <ProductionHouse />
+      <GenreMoveList />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
